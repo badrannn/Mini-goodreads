a268c2a3415d7770fb69895e341a6ca49aa2ced1
badrannn <61804347+badrannn@users.noreply.github.com>
2021-01-14 05:35:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var flag=true;
 
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 
 
 
@@ -185,3 +183,6 @@ app.post('/search',function(req,res){
   }
 })
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
